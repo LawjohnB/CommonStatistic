c344423faa7a2b2d06be5b24715453f0a97e9576
PD <lawjohn@list.ru>
2024-10-18 09:58:55 +0300

    Переименовал файл БД

--- create_tables_in_db.py
+++ create_tables_in_db.py
@@ -633,7 +633,7 @@ trips = '''
 # Функция создания таблицы в соответствии с номером недели и названием листа
 def create_table_in_db(table_name, query):
     # подключаемся к базе
-    sqlite_connection = sqlite3.connect('Все журналы.db')
+    sqlite_connection = sqlite3.connect('CommonDB.db')
     cursor = sqlite_connection.cursor()
     cursor.execute(f'''
         DROP TABLE IF EXISTS {table_name}''')
