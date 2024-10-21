8a6e8322289006315202da027db1bd9cb676ba57
PD <lawjohn@list.ru>
2024-10-21 16:38:32 +0300

    first commit

---
+++ data_to_pyxl.py
@@ -0,0 +1,257 @@
+from openpyxl import load_workbook
+import sqlite3
+from datetime import datetime
+import select_data_from_db as sdfdb
+import msvcrt
+
+start_date = datetime.strptime('08.10.2024', '%d.%m.%Y').date()
+end_date = datetime.strptime('21.10.2024', '%d.%m.%Y').date()
+start_year = datetime(datetime.now().year, 1, 1).date()
+
+
+# номер текущей недели в соответствии с выбранной датой
+current_week = datetime.isocalendar(end_date)[1]
+# в названии таблицы присутствует номер недели и текущий год
+current_year = datetime.now().year
+# название таблицы включает в себя номер недели и года
+table_prefix = '_'.join(map(str, (current_week, current_year)))
+
+
+	# словарь столбцов (columns) типов отчёта: смещение столбца относительно номера недели
+	# 2 полугодие
+# recieved - поступило материалов в неделе
+# mat_recieved - Количество уголовных дел, по которым поступили	(всех материалов)
+# primary_recieved - поступило первичных
+# additional_recieved - поступило дополнительных
+# repeated_recieved - поступило повторных
+# mats_complited - количество уголовных дел, по которым проведены (всего материалов?)
+
+colls_shift = {'recieved': -15, 'mat_recieved': 14, 'primary_recieved': 43,
+	'additional_recieved': 72, 'repeated_recieved': 101, 'comission_recieved': 130,
+	'additional_comission_recieved': 159, 'repeated_comission_recieved': 188, 
+	'complex_recieved': 217, 'additional_complex_recieved': 246, 'repeated_complex_recieved': 275,
+	'mats_complited': 331, 'complited': 307, 'complited_objs': 334,
+	'without_exec': 363, 
+	}
+
+	# Загрузка книги для чтения и записи
+	# data_only=True - считывать только значения (не формулы)
+#print('Загрузка книги')
+
+# wb = load_workbook('ЮФ_НЕД_42_II.xlsx', data_only=True)
+# ws = wb["ЭКСПЕРТИЗЫ II полугодие 2024"]
+
+
+# Заполнение строк 
+# ИАЭ - строка 18
+table_name = f'Week_{table_prefix}_ФОНО_Exps'
+human_readable_table_name = table_name.split('_')[-2]
+
+
+	## ПОСТУПИВШИЕ
+# Поступило за неделю (всего)
+recieved_on_week = sdfdb.get_recieved_exp_count(start_date, end_date, table_name)
+# Поступило с начала года (всего)
+recieved_total = sdfdb.get_recieved_exp_count(start_year, end_date, table_name)
+# Поступило за неделю материалов (уголовных дел)
+mat_recieved = sdfdb.get_recieved_mats_count(start_date, end_date, table_name)
+# Поступило за материалов (уголовных дел/МП) (всего)
+mat_recieved_total = sdfdb.get_recieved_mats_count(start_year, end_date, table_name)
+# поступило первичных (обычных) за неделю
+primary_recieved = sdfdb.get_primary_recieved_count(start_date, end_date, table_name)
+# поступило первичных (обычных) (всего)
+primary_recieved_total = sdfdb.get_primary_recieved_count(start_year, end_date, table_name)
+# поступило дополнительных за неделю
+additional_recieved = sdfdb.get_additional_recieved_count(start_date, end_date, table_name)
+# поступило дополнительных всего
+additional_recieved_total = sdfdb.get_additional_recieved_count(start_year, end_date, table_name)
+# поступило повторных
+repeated_recieved = sdfdb.get_repeated_recieved_count(start_date, end_date, table_name)
+# поступило повторных всего
+repeated_recieved_total = sdfdb.get_repeated_recieved_count(start_year, end_date, table_name)
+# поступило комиссионных (первичных)
+comission_recieved = sdfdb.get_comission_recieved_count(start_date, end_date, table_name)
+# поступило комиссионных (первичных) всего 
+comission_recieved_total = sdfdb.get_comission_recieved_count(start_year, end_date, table_name)
+# поступило комиссионных дополнительных
+additional_comission_recieved = sdfdb.get_additional_comission_recieved_count(start_date, end_date, table_name)
+# поступило комиссионных дополнительных всего 
+additional_comission_recieved_total = sdfdb.get_additional_comission_recieved_count(start_year, end_date, table_name)
+# поступило дополнительных комиссионных
+repeated_comission_recieved = sdfdb.get_repeated_comission_recieved_count(start_date, end_date, table_name)
+# поступило дополнительных комиссионных всего 
+repeated_comission_recieved_total = sdfdb.get_repeated_comission_recieved_count(start_year, end_date, table_name)
+# поступило комплексных (первичных)
+complex_recieved = sdfdb.get_complex_recieved_count(start_date, end_date, table_name)
+# поступило комплексных (первичных) всего
+complex_recieved_total = sdfdb.get_complex_recieved_count(start_year, end_date, table_name)
+# поступило комплексных дополнительных
+additional_complex_recieved = sdfdb.get_additional_complex_recieved_count(start_date, end_date, table_name)
+# поступило комплексных дополнительных
+additional_complex_recieved_total = sdfdb.get_additional_complex_recieved_count(start_year, end_date, table_name)
+# поступило комплексных повторных
+repeated_complex_recieved = sdfdb.get_repeated_complex_recieved_count(start_date, end_date, table_name)
+# поступило комплексных повторных всего 
+repeated_complex_recieved_total = sdfdb.get_repeated_complex_recieved_count(start_year, end_date, table_name)
+
+# проверка совпадения суммы поступивших
+if recieved_on_week != sum((primary_recieved, additional_recieved, repeated_recieved,
+	comission_recieved, additional_comission_recieved, repeated_comission_recieved,
+	complex_recieved, additional_complex_recieved, repeated_complex_recieved)):
+	print(f'Внимание! Проверьте количество поступивших экспертиз ({human_readable_table_name})')
+
+
+## ВЫПОЛНЕННЫЕ
+# материалов по выполненным С НАЧАЛА ГОДА!
+mats_complited = sdfdb.get_mats_complited(start_year, end_date, table_name)
+# выполнено за неделю
+complited_exps = sdfdb.get_complited(start_date, end_date, table_name)
+# выполнено всего 
+complited_exps_total = sdfdb.get_complited(start_year, end_date, table_name)
+# объектов по выполненным
+complited_objs = sdfdb.get_complited_objs(start_date, end_date, table_name)
+# объектов по выполненным всего
+complited_objs_total = sdfdb.get_complited_objs(start_year, end_date, table_name)
+
+
+		# Для выполнения данных запросов требуется Python > 3.8
+	# сроки исполненных
+status = "exp_status = 'Сдана'"
+# до 1 месяца
+terms_condition = "month = '00'" # срок до 1 месяца
+# за неделю
+complited_terms_less_month = sdfdb.get_complited_terms(start_date, end_date, table_name, status, terms_condition)
+# всего
+complited_terms_less_month_total = sdfdb.get_complited_terms(start_year, end_date, table_name, status, terms_condition)
+
+# от 1 до 2 месяцев (не включая 2)
+terms_condition = "month = '01'" # срок 1 месяц
+# за неделю
+complited_terms_1_month = sdfdb.get_complited_terms(start_date, end_date, table_name, status, terms_condition)
+# всего
+complited_terms_1_month_total = sdfdb.get_complited_terms(start_year, end_date, table_name, status, terms_condition)
+
+# от 2 до 3 месяцев (включительно)
+terms_condition = "month = '02' or month = '03'" # срок 2 или 3 месяца
+# за неделю
+complited_terms_2_3_month = sdfdb.get_complited_terms(start_date, end_date, table_name, status, terms_condition)
+# всего
+complited_terms_2_3_month_total = sdfdb.get_complited_terms(start_year, end_date, table_name, status, terms_condition)
+
+# свыше 3 месяцев
+terms_condition = "month NOT IN ('00', '01', '02', '03')" # срок более 3 месяцев (количество месяцев - строка)
+# за неделю
+complited_terms_more_3_month = sdfdb.get_complited_terms(start_date, end_date, table_name, status, terms_condition)
+# всего
+complited_terms_more_3_month_total = sdfdb.get_complited_terms(start_year, end_date, table_name, status, terms_condition)
+
+# проверка количество выполненных
+if complited_exps != sum((complited_terms_less_month, complited_terms_1_month,
+					complited_terms_2_3_month, complited_terms_more_3_month)):
+	print(f'Внимание! Проверьте количество выполненных экспертиз и сроки исполнения ({human_readable_table_name})')
+
+	# возвращено без исполнения
+without_exec = sdfdb.get_without_exec(start_date, end_date, table_name)
+without_exec_total = sdfdb.get_without_exec(start_year, end_date, table_name)
+
+# выполненные по видам
+# выполнено первичных (обычных) за неделю
+primary_completed = sdfdb.get_primary_completed_count(start_date, end_date, table_name)
+# выполнено первичных (обычных) (всего)
+primary_completed_total = sdfdb.get_primary_completed_count(start_year, end_date, table_name)
+# выполнено дополнительных за неделю
+additional_completed = sdfdb.get_additional_completed_count(start_date, end_date, table_name)
+# выполнено дополнительных всего
+additional_completed_total = sdfdb.get_additional_completed_count(start_year, end_date, table_name)
+# выполнено повторных
+repeated_completed = sdfdb.get_repeated_completed_count(start_date, end_date, table_name)
+# выполнено повторных всего
+repeated_completed_total = sdfdb.get_repeated_completed_count(start_year, end_date, table_name)
+# выполнено комиссионных (первичных)
+comission_completed = sdfdb.get_comission_completed_count(start_date, end_date, table_name)
+# выполнено комиссионных (первичных) всего 
+comission_completed_total = sdfdb.get_comission_completed_count(start_year, end_date, table_name)
+# выполнено комиссионных дополнительных
+additional_comission_completed = sdfdb.get_additional_comission_completed_count(start_date, end_date, table_name)
+# выполнено комиссионных дополнительных всего 
+additional_comission_completed_total = sdfdb.get_additional_comission_completed_count(start_year, end_date, table_name)
+# выполнено дополнительных комиссионных
+repeated_comission_completed = sdfdb.get_repeated_comission_completed_count(start_date, end_date, table_name)
+# выполнено дополнительных комиссионных всего 
+repeated_comission_completed_total = sdfdb.get_repeated_comission_completed_count(start_year, end_date, table_name)
+# выполнено комплексных (первичных)
+complex_completed = sdfdb.get_complex_completed_count(start_date, end_date, table_name)
+# выполнено комплексных (первичных) всего
+complex_completed_total = sdfdb.get_complex_completed_count(start_year, end_date, table_name)
+# выполнено комплексных дополнительных
+additional_complex_completed = sdfdb.get_additional_complex_completed_count(start_date, end_date, table_name)
+# выполнено комплексных дополнительных
+additional_complex_completed_total = sdfdb.get_additional_complex_completed_count(start_year, end_date, table_name)
+# выполнено комплексных повторных
+repeated_complex_completed = sdfdb.get_repeated_complex_completed_count(start_date, end_date, table_name)
+# выполнено комплексных повторных всего 
+repeated_complex_completed_total = sdfdb.get_repeated_complex_completed_count(start_year, end_date, table_name)
+
+# проверка совпадения суммы выполненных
+if complited_exps != sum((primary_completed, additional_completed, repeated_completed,
+    comission_completed, additional_comission_completed, repeated_comission_completed,
+    complex_completed, additional_complex_completed, repeated_complex_completed)):
+	print(f'Внимание! Проверьте количество выполненных экспертиз и их типы ({human_readable_table_name})')
+
+# НПВ
+# за неделю
+npv = sdfdb.get_npv_count(start_date, end_date, table_name)
+# всего
+npv_total = sdfdb.get_npv_count(start_year, end_date, table_name)
+
+# установлено лиц
+persons_est = sdfdb.get_persons_est_count(start_date, end_date, table_name)
+persons_est_total = sdfdb.get_persons_est_count(start_year, end_date, table_name)
+
+# установлено факторов
+facts_est = sdfdb.get_facts_est_count(start_date, end_date, table_name)
+facts_est_total = sdfdb.get_facts_est_count(start_year, end_date, table_name)
+
+	# В ПРОИЗВОДСТВЕ
+# всего экспертиз
+exps_in_work = sdfdb.get_work_count(table_name)
+# всего объектов
+objs_in_work = sdfdb.get_work_objs_count(table_name)
+
+# сроки по находящимся в работе
+# end_date - дата отчёта, на которую производится расчёт
+status = "exp_status = 'В производстве'"
+# до 1 месяца
+terms_condition = "month = '00'" # срок до 1 месяца
+in_work_terms_less_month = sdfdb.get_work_terms(end_date, table_name, status, terms_condition)
+# от 1 до 2 месяцев (не включая 2)
+terms_condition = "month = '01'" # срок 1 месяц
+in_work_terms_1_month = sdfdb.get_work_terms(end_date, table_name, status, terms_condition)
+# от 2 до 3 месяцев (включительно)
+terms_condition = "month = '02' or month = '03'" # срок 2 или 3 месяца
+in_work_terms_2_3_month = sdfdb.get_work_terms(end_date, table_name, status, terms_condition)
+# свыше 3 месяцев
+terms_condition = "month NOT IN ('00', '01', '02', '03')" # срок более 3 месяцев (количество месяцев - строка)
+in_work_terms_more_3_month = sdfdb.get_work_terms(end_date, table_name, status, terms_condition)
+
+# проверка количество выполненных
+if exps_in_work != sum((in_work_terms_less_month, in_work_terms_1_month, in_work_terms_2_3_month,
+				in_work_terms_more_3_month)):
+	print(f'Внимание! Проверьте количество выполненных экспертиз и сроки исполнения ({human_readable_table_name})')
+
+
+# print("Press any key to continue...")
+# msvcrt.getch()
+	
+
+# for row in temp_rows:
+# 	print(f'Заполнение строки {temp_rows[row]}')
+# 	coll = current_week - 15
+# 	ws.cell(row=row, column=coll).value = f'{temp_rows[row]} поступило'
+# 	coll = current_week + 14
+# 	ws.cell(row=row, column=coll).value = f'{temp_rows[row]} УД поступило'
+# 	coll = current_week + 43
+# 	ws.cell(row=row, column=coll).value = f'{temp_rows[row]} первичных поступило'
+
+# print('Сохранение данных')
+# wb.save("TEST.xlsx") 
---
+++ select_data_from_db.py
@@ -0,0 +1,428 @@
+import sqlite3
+from datetime import datetime
+
+# Фукнция обработки ошибок в файл
+def log_data(data, file_name):
+     with open(file_name, 'a', encoding='UTF-8') as file:
+          file.write(str(datetime.now()))
+          file.write('\n')
+          file.write(data)
+          file.write('\n\n')
+
+# функция отбора данных из базы
+# с указанием отбора одной записи или всех
+def query_data(query, fetch='one'):
+    sqlite_connection = sqlite3.connect('CommonDB.db')
+    cursor = sqlite_connection.cursor()
+    sqlite_select_query = query
+    cursor.execute(sqlite_select_query)
+    if fetch == 'one':
+        query_result = cursor.fetchone()[0]
+    else:
+        query_result = cursor.fetchall()
+    cursor.close()
+    sqlite_connection.close()
+    # почему-то приходит кортеж с одним значением 
+    return query_result
+
+ # поступившие
+# отбор поступивших экспертиз (всего)
+def get_recieved_exp_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_in_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# поступило материалов
+def get_recieved_mats_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT mat_number)
+    FROM {table_name}
+    WHERE exp_in_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# поступило первичных
+def get_primary_recieved_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Первичная'
+    AND exp_in_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# поступило дополнительных
+def get_additional_recieved_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Дополнительная'
+    AND exp_in_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# поступило повторных
+def get_repeated_recieved_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Повторная'
+    AND exp_in_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# поступило комиссионных 
+def get_comission_recieved_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Комиссионная'
+    AND exp_in_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# поступило дополнительных комиссионных
+def get_additional_comission_recieved_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Дополнительная комиссионная'
+    AND exp_in_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# поступило повторных комиссионных
+def get_repeated_comission_recieved_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Повторная комиссионная'
+    AND exp_in_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# поступило комплексных
+def get_complex_recieved_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Комплексная'
+    AND exp_in_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# поступило дополнительных комплексных
+def get_additional_complex_recieved_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Дополнительная комплексная'
+    AND exp_in_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# поступило повторных комплексных
+def get_repeated_complex_recieved_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Повторная комплексная'
+    AND exp_in_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+    # выполненные
+# материалов по выполненным за весь год
+def get_mats_complited(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT mat_number)
+    FROM {table_name}
+    WHERE exp_status = 'Сдана'
+    AND exp_end_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# всего выполнено
+def get_complited(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_status = 'Сдана'
+    AND exp_end_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# объектов по выполненным
+def get_complited_objs(start_date, end_date, table_name):
+    # решается через табличное выражение
+    # для отбора уникальных номеров экспертиз/исследований
+    # в которых выбирается максимальный показатель объектов
+    # и в основном запросе берётся сумма
+    query = f'''
+    WITH get_uniq_objs_by_exps AS 
+        (SELECT DISTINCT exp_number, MAX(objs_count) AS objs_count
+        FROM {table_name}
+        WHERE exp_status = 'Сдана'
+        AND exp_end_date
+        BETWEEN '{start_date}' AND '{end_date}'
+        GROUP BY exp_number
+        ORDER BY objs_count DESC)
+    SELECT SUM(objs_count) FROM get_uniq_objs_by_exps
+    '''
+    return query_data(query)
+
+# возвращённых без исполнения
+def get_without_exec(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_status = 'Без исполнения'
+    AND exp_end_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+    # Для выполнения данного запроса требуется Python > 3.8
+# исполненные сроки (все)
+# запрос отберёт сроки исполнения в формате строки (+0000-00-22 00:00:00.000),
+# из которой отберёт подстроку с месяцем
+# в итоге запрос вернёт таблицу
+def get_complited_terms(start_date, end_date, table_name, status, terms_condition):
+    # запрос через табличное выражение работает некорректно
+    # query = f'''
+    # WITH get_month AS 
+    #     (SELECT
+    #     SUBSTRING(TIMEDIFF(exp_end_date, exp_in_date), 7, 2) AS month
+    #     FROM {table_name}
+    #      WHERE exp_status = 'Сдана'
+    #     AND exp_end_date
+    #     BETWEEN '{start_date}' AND '{end_date}'
+    #     GROUP BY exp_number)
+    # SELECT COUNT(*) AS month_count
+    # FROM get_month
+    # HAVING month = '01'
+    # GROUP BY month  
+    # '''
+    query = f'''SELECT COUNT(month)
+        FROM 
+          (SELECT
+            SUBSTRING(TIMEDIFF(exp_end_date, exp_in_date), 7, 2) AS month
+            FROM  {table_name}
+            WHERE {status}
+            AND exp_end_date
+            BETWEEN '{start_date}' AND '{end_date}'
+            GROUP BY exp_number)
+        WHERE {terms_condition}
+        GROUP BY month
+    '''
+    # ??? запрос не работает с fetchone(), если указать условие WHERE 
+    # fetchall нужен для отбора всех записей
+    res = query_data(query, fetch='all')
+    return sum((x[0]) for x in res)
+
+
+# выполненные по видам
+# выполнено первичных
+def get_primary_completed_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Первичная'
+    AND exp_status = 'Сдана'
+    AND exp_end_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# выполнено дополнительных
+def get_additional_completed_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Дополнительная'
+    AND exp_status = 'Сдана'
+    AND exp_end_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# выполнено повторных
+def get_repeated_completed_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Повторная'
+    AND exp_status = 'Сдана'
+    AND exp_end_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# выполнено комиссионных 
+def get_comission_completed_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Комиссионная'
+    AND exp_status = 'Сдана'
+    AND exp_end_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# выполнено дополнительных комиссионных
+def get_additional_comission_completed_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Дополнительная комиссионная'
+    AND exp_end_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# выполнено повторных комиссионных
+def get_repeated_comission_completed_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Повторная комиссионная'
+    AND exp_status = 'Сдана'
+    AND exp_end_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# выполнено комплексных
+def get_complex_completed_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Комплексная'
+    AND exp_status = 'Сдана'
+    AND exp_end_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# выполнено дополнительных комплексных
+def get_additional_complex_completed_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Дополнительная комплексная'
+    AND exp_status = 'Сдана'
+    AND exp_end_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# выполнено повторных комплексных
+def get_repeated_complex_completed_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_type = 'Повторная комплексная'
+    AND exp_status = 'Сдана'
+    AND exp_end_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+
+# НПВ
+# выполнено повторных комплексных
+def get_npv_count(start_date, end_date, table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE 
+    exp_result = 'НПВ'
+    AND exp_end_date
+    BETWEEN '{start_date}' AND '{end_date}'
+    '''
+    return query_data(query)
+
+# установлено возможно причастных лиц
+def get_persons_est_count(start_date, end_date, table_name):
+    query = f'''
+    WITH get_pers_est AS
+        (SELECT exp_number, MAX(crime_persons_est) AS p_est
+        FROM {table_name}
+        WHERE exp_end_date
+        BETWEEN '{start_date}' AND '{end_date}'
+        GROUP BY exp_number)
+    SELECT SUM(p_est)
+    FROM get_pers_est    
+    '''
+    return query_data(query)
+
+
+def get_facts_est_count(start_date, end_date, table_name):
+    query = f'''
+    WITH get_facts_est AS
+        (SELECT exp_number, MAX(facts_est) AS f_est
+        FROM {table_name}
+        WHERE exp_end_date 
+        BETWEEN '{start_date}' AND '{end_date}'
+        GROUP BY exp_number)
+    SELECT SUM(f_est)
+    FROM get_facts_est  
+    '''
+    return query_data(query)
+
+
+def get_work_count(table_name):
+    query = f'''
+    SELECT COUNT(DISTINCT exp_number)
+    FROM {table_name}
+    WHERE exp_status = 'В производстве'
+    '''
+    return query_data(query)
+
+def get_work_objs_count(table_name):
+    query = f'''
+    WITH get_uniq_objs_by_exps AS 
+        (SELECT DISTINCT exp_number, MAX(objs_count) AS objs_count
+        FROM {table_name}
+        WHERE exp_status = 'В производстве'
+        GROUP BY exp_number
+        ORDER BY objs_count DESC)
+    SELECT SUM(objs_count) FROM get_uniq_objs_by_exps
+    '''
+    return query_data(query)
+
+
+# Для выполнения данного запроса требуется Python > 3.8
+# сроки в производстве
+def get_work_terms(end_date, table_name, status, terms_condition):
+    query = f'''SELECT COUNT(month)
+        FROM 
+          (SELECT
+            SUBSTRING(TIMEDIFF({end_date}, exp_in_date), 7, 2) AS month
+            FROM  {table_name}
+            WHERE {status}
+            GROUP BY exp_number)
+        WHERE {terms_condition}
+        GROUP BY month
+    '''
+    # ??? запрос не работает с fetchone(), если указать условие WHERE 
+    # fetchall нужен для отбора всех записей
+    res = query_data(query, fetch='all')
+    return sum((x[0]) for x in res)
